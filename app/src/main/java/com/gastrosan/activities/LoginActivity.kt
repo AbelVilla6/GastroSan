31d5d5bcbe792d3482f7d26f344a575132467e9b
AbelVilla6 <Abel.ortiz1@alu.uclm.es>
2024-03-26 19:52:02 +0100

    Log in Google

--- app/src/main/java/com/gastrosan/activities/LoginActivity.kt
+++ app/src/main/java/com/gastrosan/activities/LoginActivity.kt
@@ -15,7 +15,16 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gastrosan.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.FirebaseDatabase
 import java.util.concurrent.Executor
 
 
@@ -26,12 +35,21 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var inputPassword: EditText
     private lateinit var checkBox: CheckBox
 
+    private lateinit var bGoogle: Button
+
     var mLoadingBar: ProgressDialog? = null
 
     private val executor: Executor? = null
     private val biometricPrompt: BiometricPrompt? = null
     //private val promptInfo: BiometricPrompt.PromptInfo? = null
     var mAuth: FirebaseAuth? = null
+    var database: FirebaseDatabase? = null
+
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    val RC_SIGN_IN = 20
+
+
 
     @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,6 +70,8 @@ class LoginActivity : AppCompatActivity() {
         }
         })
 
+        bGoogle = findViewById(R.id.button2)
+
 
         bLogIn.setOnClickListener(View.OnClickListener {
             vibrator.vibrate(100)
@@ -63,7 +83,62 @@ class LoginActivity : AppCompatActivity() {
         inputPassword = findViewById<EditText>(R.id.editText2)
         mAuth = FirebaseAuth.getInstance()
         mLoadingBar = ProgressDialog(this@LoginActivity)
+        database = FirebaseDatabase.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+//            .requestProfile()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        bGoogle.setOnClickListener {
+            googleSingIn()
+        }
     }
+
+    private fun googleSingIn() {
+        val intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth?.signInWithCredential(credential)?.addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                val user = mAuth?.currentUser
+                val map = HashMap<String, Any>()
+                map["id"] = user!!.uid
+                map["name"] = user.displayName ?: ""
+                //map.put("profile",user.photoUrl.toString())
+
+                database?.reference?.child("Users")?.child(user.uid)?.setValue(map)
+
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this@LoginActivity, "Algo fue mal", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+                account?.idToken?.let { firebaseAuthWithGoogle(it) }
+            } catch (e: ApiException) {
+                Toast.makeText(this@LoginActivity, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 
     private fun checkCredentials(): Boolean {
         val result = false
